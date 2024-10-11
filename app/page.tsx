85fd4e9bb03309ddeef4cd64e720a48ee55c93d7
Chris Deck <deckchris95@gmail.com>
2024-10-11 14:37:38 -0400

    sorting, filtering, aggregate table data, display g without hover

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -35,7 +35,14 @@ import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { toast } from "@/hooks/use-toast";
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  LabelList,
+  XAxis,
+  YAxis,
+} from "recharts";
 import {
   Card,
   CardContent,
@@ -48,6 +55,64 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+import { DataTableColumnHeader } from "./components/data-table-column-header";
+
+const columns: ColumnDef<Filament>[] = [
+  {
+    accessorKey: "id",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="ID" />
+    ),
+    cell: ({ row }) => <div>{row.getValue("id")}</div>,
+  },
+  {
+    accessorKey: "brand",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Brand" />
+    ),
+    cell: ({ row }) => <div>{row.getValue("brand")}</div>,
+  },
+  {
+    accessorKey: "material",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Material" />
+    ),
+    cell: ({ row }) => <div>{row.getValue("material")}</div>,
+  },
+  {
+    accessorKey: "color",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Color" />
+    ),
+    cell: ({ row }) => <div>{row.getValue("color")}</div>,
+  },
+  {
+    accessorKey: "weight",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Weight" />
+    ),
+    cell: ({ row }) => <div>{row.getValue("weight")}</div>,
+  },
+];
 
 export default function Home() {
   const [filaments, setFilaments] = useState<Filament[]>([]);
@@ -62,6 +127,42 @@ export default function Home() {
   const router = useRouter();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [globalFilter, setGlobalFilter] = useState("");
+
+  const table = useReactTable({
+    data: filaments,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    globalFilterFn: (row, columnId, filterValue) => {
+      const value = row.getValue(columnId);
+      if (typeof value === "string" || typeof value === "number") {
+        setSearchTerm(filterValue);
+        return String(value)
+          .toLowerCase()
+          .includes(String(filterValue).toLowerCase());
+      }
+      return false;
+    },
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+      globalFilter,
+    },
+    onGlobalFilterChange: setGlobalFilter,
+  });
 
   useEffect(() => {
     fetchFilaments();
@@ -131,12 +232,33 @@ export default function Home() {
     )
   );
 
-  // Transforming the `filaments` array to meet the requirement of having string `id` values
-  const transformedFilaments = filteredFilaments.map((filament) => ({
-    ...filament,
-    id: filament.id.toString(), // Convert id from number to string
-    label: `${filament.brand} ${filament.material} ${filament.color}`, // Create the concatenated label
-  }));
+  const combinedFilaments = useMemo(() => {
+    const combined: { [key: string]: Filament & { totalWeight: number } } = {};
+    filaments.forEach((filament) => {
+      const key = `${filament.brand}-${filament.material}-${filament.color}`;
+      if (combined[key]) {
+        combined[key].totalWeight += filament.weight || 0;
+      } else {
+        combined[key] = { ...filament, totalWeight: filament.weight || 0 };
+      }
+    });
+    return Object.values(combined);
+  }, [filaments]);
+
+  // // Transforming the `filaments` array to meet the requirement of having string `id` values
+  // const transformedFilaments = filteredFilaments.map((filament) => ({
+  //   ...filament,
+  //   id: filament.id.toString(), // Convert id from number to string
+  //   label: `${filament.brand} ${filament.material} ${filament.color}`, // Create the concatenated label
+  // }));
+
+  const transformedFilaments = useMemo(() => {
+    return combinedFilaments.map((filament) => ({
+      id: filament.id,
+      label: `${filament.brand} ${filament.material} ${filament.color}`,
+      weight: filament.totalWeight,
+    }));
+  }, [combinedFilaments]);
 
   return (
     <div className="container mx-auto p-4">
@@ -171,7 +293,7 @@ export default function Home() {
                   position: "insideBottom",
                   offset: -5,
                 }}
-                domain={[0, 1100]} // Set range from 0 to 1100g
+                domain={[0, "auto"]} // Set range from 0 to auto
               />
               <YAxis
                 type="category"
@@ -182,20 +304,27 @@ export default function Home() {
                 textAnchor="end"
               />
               <ChartTooltip content={<ChartTooltipContent />} />
-              <Bar dataKey="weight" fill="var(--color-weight)" />
+              <Bar dataKey="weight" fill="var(--color-weight)">
+                <LabelList
+                  dataKey="weight"
+                  position="right"
+                  formatter={(value: number) => `${value}g`}
+                  style={{ fill: "var(--color-weight)", fontSize: "12px" }}
+                />
+              </Bar>
             </BarChart>
           </ChartContainer>
         </CardContent>
       </Card>
 
       <div className="mb-4 flex flex-col sm:flex-row justify-between items-center gap-4">
-        <Input
+        {/* <Input
           type="text"
           placeholder="Search filaments..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           className="w-full sm:max-w-sm"
-        />
+        /> */}
 
         <Dialog open={isAddDialogOpen} onOpenChange={setIsAddDialogOpen}>
           <DialogTrigger asChild>
@@ -290,53 +419,125 @@ export default function Home() {
         </Dialog>
       </div>
 
-      <div className="overflow-x-auto mx-auto">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Search brands, colors, materials..."
+          value={globalFilter}
+          onChange={(event) => setGlobalFilter(event.target.value)}
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
         <Table>
           <TableHeader>
-            <TableRow>
-              <TableHead>ID</TableHead>
-              <TableHead>Brand</TableHead>
-              <TableHead>Material</TableHead>
-              <TableHead>Color</TableHead>
-              <TableHead>Weight (g)</TableHead>
-            </TableRow>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
           </TableHeader>
           <TableBody>
-            {isLoading
-              ? Array.from({ length: 5 }).map((_, index) => (
-                  <TableRow key={index}>
-                    <TableCell>
+            {isLoading ? (
+              Array.from({ length: 5 }).map((_, index) => (
+                <TableRow key={index}>
+                  {columns.map((column, cellIndex) => (
+                    <TableCell key={cellIndex}>
                       <Skeleton className="w-full h-6" />
                     </TableCell>
-                    <TableCell>
-                      <Skeleton className="w-full h-6" />
+                  ))}
+                </TableRow>
+              ))
+            ) : table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  onClick={goToFilament(row.original.id)}
+                  className="cursor-pointer"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
                     </TableCell>
-                    <TableCell>
-                      <Skeleton className="w-full h-6" />
-                    </TableCell>
-                    <TableCell>
-                      <Skeleton className="w-full h-6" />
-                    </TableCell>
-                    <TableCell>
-                      <Skeleton className="w-full h-6" />
-                    </TableCell>
-                  </TableRow>
-                ))
-              : filteredFilaments.map((filament) => (
-                  <TableRow
-                    key={filament.id}
-                    onClick={goToFilament(filament.id)}
-                    className="cursor-pointer"
-                  >
-                    <TableCell>{filament.id}</TableCell>
-                    <TableCell>{filament.brand}</TableCell>
-                    <TableCell>{filament.material}</TableCell>
-                    <TableCell>{filament.color}</TableCell>
-                    <TableCell>{filament.weight}</TableCell>
-                  </TableRow>
-                ))}
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   );
