ac349244efa8b3f1ee524eb8e811b2791a71cb8b
Chris Deck <deckchris95@gmail.com>
2024-10-15 08:40:49 -0400

    mobile filtering css

--- app/components/data-table-toolbar.tsx
+++ app/components/data-table-toolbar.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { DataTableViewOptions } from "./data-table-view-options";
 import { DataTableFacetedFilter } from "./data-table-faceted-filter";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Color } from "../types/Color";
 
 interface DataTableToolbarProps<TData> {
@@ -87,57 +87,142 @@ export function DataTableToolbar<TData>({
     };
   });
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      // console.log("width: ", width);
+      setIsMobile(width <= 1080);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="flex items-center justify-between">
-      <div className="flex flex-1 items-center space-x-2">
-        <Input
-          placeholder="Search brands, colors, materials..."
-          value={globalFilter}
-          onChange={(event) => {
-            setGlobalFilter(event.target.value);
-            setSearchTerm(event.target.value);
-          }}
-          className="max-w-sm"
-        />
-        {table.getColumn("brand") && brandOptions.length > 0 && (
-          <DataTableFacetedFilter
-            column={table.getColumn("brand")}
-            title="Brand"
-            options={brandOptions}
-            globalFilter={globalFilter}
-            setGlobalFilter={setGlobalFilter}
-            setSearchTerm={setSearchTerm}
-          />
-        )}
-        {table.getColumn("material") && materialOptions.length > 0 && (
-          <DataTableFacetedFilter
-            column={table.getColumn("material")}
-            title="Material"
-            options={materialOptions}
-            globalFilter={globalFilter}
-            setGlobalFilter={setGlobalFilter}
-            setSearchTerm={setSearchTerm}
-          />
-        )}
-        {table.getColumn("color") && colorOptions.length > 0 && (
-          <DataTableFacetedFilter
-            column={table.getColumn("color")}
-            title="Color"
-            options={colorOptions}
-            globalFilter={globalFilter}
-            setGlobalFilter={setGlobalFilter}
-            setSearchTerm={setSearchTerm}
-          />
-        )}
-        {isFiltered && (
-          <Button
-            variant="ghost"
-            onClick={() => table.resetColumnFilters()}
-            className="h-8 px-2 lg:px-3"
-          >
-            Reset
-            <Cross2Icon className="ml-2 h-4 w-4" />
-          </Button>
+      <div
+        className={
+          isMobile
+            ? "flex flex-1 flex-col items-start gap-2"
+            : "flex flex-1 flex-col items-center"
+        }
+      >
+        {isMobile ? (
+          <>
+            <div className="flex flex-row items-start w-full">
+              <Input
+                placeholder="Search brands, colors, materials..."
+                value={globalFilter}
+                onChange={(event) => {
+                  setGlobalFilter(event.target.value);
+                  setSearchTerm(event.target.value);
+                }}
+                className="max-w-sm"
+              />
+            </div>
+            <div className="flex flex-row items-start w-full gap-4">
+              {table.getColumn("brand") && brandOptions.length > 0 && (
+                <DataTableFacetedFilter
+                  column={table.getColumn("brand")}
+                  title="Brand"
+                  options={brandOptions}
+                  globalFilter={globalFilter}
+                  setGlobalFilter={setGlobalFilter}
+                  setSearchTerm={setSearchTerm}
+                />
+              )}
+              {table.getColumn("material") && materialOptions.length > 0 && (
+                <DataTableFacetedFilter
+                  column={table.getColumn("material")}
+                  title="Material"
+                  options={materialOptions}
+                  globalFilter={globalFilter}
+                  setGlobalFilter={setGlobalFilter}
+                  setSearchTerm={setSearchTerm}
+                />
+              )}
+            </div>
+            <div className="flex flex-row items-start">
+              {table.getColumn("color") && colorOptions.length > 0 && (
+                <DataTableFacetedFilter
+                  column={table.getColumn("color")}
+                  title="Color"
+                  options={colorOptions}
+                  globalFilter={globalFilter}
+                  setGlobalFilter={setGlobalFilter}
+                  setSearchTerm={setSearchTerm}
+                />
+              )}
+              {isFiltered && (
+                <Button
+                  variant="ghost"
+                  onClick={() => table.resetColumnFilters()}
+                  className="h-8 px-2 lg:px-3"
+                >
+                  Reset
+                  <Cross2Icon className="ml-2 h-4 w-4" />
+                </Button>
+              )}
+            </div>
+          </>
+        ) : (
+          <div className="flex flex-row items-center space-x-2">
+            <Input
+              placeholder="Search brands, colors, materials..."
+              value={globalFilter}
+              onChange={(event) => {
+                setGlobalFilter(event.target.value);
+                setSearchTerm(event.target.value);
+              }}
+              className="max-w-sm"
+            />
+            {table.getColumn("brand") && brandOptions.length > 0 && (
+              <DataTableFacetedFilter
+                column={table.getColumn("brand")}
+                title="Brand"
+                options={brandOptions}
+                globalFilter={globalFilter}
+                setGlobalFilter={setGlobalFilter}
+                setSearchTerm={setSearchTerm}
+              />
+            )}
+            {table.getColumn("material") && materialOptions.length > 0 && (
+              <DataTableFacetedFilter
+                column={table.getColumn("material")}
+                title="Material"
+                options={materialOptions}
+                globalFilter={globalFilter}
+                setGlobalFilter={setGlobalFilter}
+                setSearchTerm={setSearchTerm}
+              />
+            )}
+            {table.getColumn("color") && colorOptions.length > 0 && (
+              <DataTableFacetedFilter
+                column={table.getColumn("color")}
+                title="Color"
+                options={colorOptions}
+                globalFilter={globalFilter}
+                setGlobalFilter={setGlobalFilter}
+                setSearchTerm={setSearchTerm}
+              />
+            )}
+            {isFiltered && (
+              <Button
+                variant="ghost"
+                onClick={() => table.resetColumnFilters()}
+                className="h-8 px-2 lg:px-3"
+              >
+                Reset
+                <Cross2Icon className="ml-2 h-4 w-4" />
+              </Button>
+            )}
+          </div>
         )}
       </div>
       <DataTableViewOptions table={table} columnHeaders={columnHeadersArray} />
